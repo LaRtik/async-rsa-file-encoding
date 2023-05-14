db90be30499aac6411a9799e040922d06180c108
vKiiNGz <lolkanalolke@mail.ru>
2023-05-15 01:53:48 +0300

    trash

--- rsa.cpp
+++ rsa.cpp
@@ -84,20 +84,6 @@ RSA::encryptMessage(std::vector<largeIntegerType> data,
   clock_t start, end;
   start = clock();
   std::vector<largeIntegerType> encryptedMessage;
-<<<<<<< HEAD
-  std::vector<std::future<largeIntegerType>> futures;
-
-   for (largeIntegerType element : data) {
-      if (futures.size() > 10) {
-          for (int i = 0; i < int(futures.size()); i++) {
-              auto res = futures[i].get();
-              encryptedMessage.push_back(res);
-          }
-          futures.clear();
-      }
-      futures.push_back(
-          std::async(std::launch::async, &RSA::encryptData, this, element, _privateKey));
-=======
   std::vector<std::future<std::vector<largeIntegerType> > > futures;
 
   int block_size = data.size() / 10;
@@ -108,28 +94,18 @@ RSA::encryptMessage(std::vector<largeIntegerType> data,
           r = data.size() - 1;
         futures.push_back(
             std::async(std::launch::async, &RSA::encr, this, data, l, r, _privateKey));
->>>>>>> 1eea95bf1f8a0df052a0b9fc2bbfd09f8de70da5
+
   }
-
-
-
   for (int i = 0; i < int(futures.size()); i++) {
       auto res = futures[i].get();
       for (const auto &el : res) encryptedMessage.push_back(el);
   }
 
-<<<<<<< HEAD
   /*
   for (const auto &d : data) {
         encryptedMessage.push_back(encryptData(d, _privateKey));
   }
 */
-=======
-  /*for (const auto &d : data) {
-        encryptedMessage.push_back(encryptData(d, _privateKey));
-  }*/
-
->>>>>>> 1eea95bf1f8a0df052a0b9fc2bbfd09f8de70da5
   end = clock();
   qDebug() << double(end - start) / CLOCKS_PER_SEC;
 
